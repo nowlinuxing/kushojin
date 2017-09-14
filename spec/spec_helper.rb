50c7fbdd22a3b16cd7a5e85cd681698cd2b2e4f3
Loose coupling <loosecplg@gmail.com>
2017-09-14 13:44:27 +0900

    Connect to DB for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require "bundler/setup"
+require "active_record"
 require "kushojin"
 require "pry-byebug"
+
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+# ActiveRecord::Base.logger = ::Logger.new(STDOUT)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
