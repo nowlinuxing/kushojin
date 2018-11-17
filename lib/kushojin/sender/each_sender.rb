66e0f3afd675c729c088ab71622ac6e40dd23ede
Loose coupling <loosecplg@gmail.com>
2018-11-18 00:39:18 +0900

    Remove duplicated code

--- lib/kushojin/sender/each_sender.rb
+++ lib/kushojin/sender/each_sender.rb
@@ -17,17 +17,6 @@ module Kushojin
       def tag(controller)
         "#{controller.controller_name}.#{controller.action_name}"
       end
-
-      def map(change, controller:)
-        changes = change.changes_without_primary_key.reject { |attr_name, _| ignore_columns.include?(attr_name) }
-        {
-          "event"            => change.event.to_s,
-          "request_id"       => controller.request.request_id,
-          "table_name"       => change.table_name,
-          change.primary_key => change.model.id,
-          "changes"          => changes,
-        }
-      end
     end
   end
 end
