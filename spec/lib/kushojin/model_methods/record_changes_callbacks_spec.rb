6e02e1fc72aa0fdef3fe36852fc617aab8ace4d3
Loose coupling <loosecplg@gmail.com>
2018-11-17 12:20:44 +0900

    Extract contexts and examples

--- spec/lib/kushojin/model_methods/record_changes_callbacks_spec.rb
+++ spec/lib/kushojin/model_methods/record_changes_callbacks_spec.rb
@@ -10,6 +10,34 @@ RSpec.describe Kushojin::ModelMethods::RecordChangesCallbacks do
       end
     end
 
+    shared_context "create a record" do
+      before { Kushojin::Recorder.build }
+      subject(:change_record) { model.create(name: "bill", age: 20) }
+    end
+
+    shared_context "update a record" do
+      before do
+        model.create(name: "bill", age: 20)
+        Kushojin::Recorder.build
+      end
+
+      subject(:change_record) { model.take.update(name: "bob", age: 21) }
+    end
+
+    shared_context "destroy a record" do
+      before do
+        model.create(name: "bill", age: 20)
+        Kushojin::Recorder.build
+      end
+
+      subject(:change_record) { model.take.destroy }
+    end
+
+    shared_examples "record a change" do |event|
+      it { expect { change_record }.to change(Kushojin::Recorder.changes, :size).by(1) }
+      it { expect(recorded_change.event).to eq(event) }
+    end
+
     after do
       model.delete_all
       Kushojin::Recorder.destroy
@@ -21,35 +49,18 @@ RSpec.describe Kushojin::ModelMethods::RecordChangesCallbacks do
     end
 
     context "when create a record" do
-      before { Kushojin::Recorder.build }
-      subject(:change_record) { model.create(name: "bill", age: 20) }
-
-      it { expect { change_record }.to change(Kushojin::Recorder.changes, :size).by(1) }
-      it { expect(recorded_change.event).to eq(:create) }
+      include_context "create a record"
+      include_examples "record a change", :create
     end
 
     context "when update attributes" do
-      before do
-        model.create(name: "bill", age: 20)
-        Kushojin::Recorder.build
-      end
-
-      subject(:change_record) { model.take.update(name: "bob", age: 21) }
-
-      it { expect { change_record }.to change(Kushojin::Recorder.changes, :size).by(1) }
-      it { expect(recorded_change.event).to eq(:update) }
+      include_context "update a record"
+      include_examples "record a change", :update
     end
 
     context "when delete a record" do
-      before do
-        model.create(name: "bill", age: 20)
-        Kushojin::Recorder.build
-      end
-
-      subject(:change_record) { model.take.destroy }
-
-      it { expect { change_record }.to change(Kushojin::Recorder.changes, :size).by(1) }
-      it { expect(recorded_change.event).to eq(:destroy) }
+      include_context "destroy a record"
+      include_examples "record a change", :destroy
     end
   end
 end
