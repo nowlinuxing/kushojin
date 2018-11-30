a979a1de76c27eceaf983f4aa7a1054df860804a
Loose coupling <loosecplg@gmail.com>
2018-12-01 01:57:24 +0900

    Add a comment for #send_changes

--- lib/kushojin/controller_methods/callback.rb
+++ lib/kushojin/controller_methods/callback.rb
@@ -1,6 +1,49 @@
 module Kushojin
   module ControllerMethods
     module Callback
+      # Send recorded changes.
+      #
+      #   class UsersController < ApplicationController
+      #     send_changes
+      #
+      #     def create
+      #       User.create(user_params)
+      #     end
+      #
+      #     def update
+      #       User.find(params[:id]).update(user_params)
+      #     end
+      #
+      #     def destroy
+      #       User.find(params[:id]).destroy
+      #     end
+      #
+      #     private
+      #
+      #     def user_params
+      #       params.require(:user).permit(:name)
+      #     end
+      #   end
+      #
+      # You can pass in a class or an instance to change behavior of the callback.
+      #
+      #   class CustomCallback < Kushojin::ControllerMethods::SendChangeCallback
+      #     # Must respond to around.
+      #     def around(controller)
+      #       # Do something
+      #       super
+      #     end
+      #   end
+      #
+      #   class UsersController < ApplicationController
+      #     send_changes CustomCallback.new
+      #   end
+      #
+      # ===== Options
+      #
+      # * <tt>only</tt> - Send changes only for this action.
+      # * <tt>except</tt> - Send changes for all actions except this action.
+      #
       def send_changes(callback = nil, **options)
         callback ||= Kushojin::ControllerMethods::SendChangeCallback.new
         around_action callback, options
